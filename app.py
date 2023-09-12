0bdce23a342dd119451aab7da556d06bf555a6c3
Staycia930 <skb.ceg.biblia@gmail.com>
2023-09-12 15:38:38 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+import datetime
+
+app = Flask(__name__)
+
+database = {
+    "北館":{},
+    "南館":{
+        "1A":["1-A", "混雑していない", ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))],
+        "1B":["1-B", "混雑していない", ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))],
+        "1C":["1-C", "混雑していない", ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))],
+        "1D":["1-D", "混雑していない", ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))],
+        "1E":["1-E", "混雑していない", ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))],
+        "1F":["1-F", "混雑していない", ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))],
+        "1G":["1-G", "混雑していない", ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))],
+    },
+    "三号館":{}
+}
+
+
+@app.route("/<tabname>")
+def main(tabname):
+    return render_template("index.html", tabname=tabname, data=database[tabname])
+
+@app.route("/update/<tabname>", methods=["GET"])
+def update(tabname):
+    _class = request.args.get("class")
+    congestion = request.args.get("congestion")
+    print(_class, congestion)
+    if _class != None and congestion != None:
+        database[tabname][_class][1] = congestion
+        database[tabname][_class][2] = ":".join(map(str, [datetime.datetime.now().hour, datetime.datetime.now().minute]))
+        print(database)
+    return render_template("update.html",tabname=tabname, data=database[tabname])
+
+@app.route("/")
+def redi():
+    return redirect("/北館")
+
+
+if __name__ == "__main__":
+    app.run(port=8000, host="0.0.0.0", debug=True)
